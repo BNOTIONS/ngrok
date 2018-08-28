41bc0742ecf5172578c319c4748cbf30663bfec3
Jonathon W. Marshall <jonathon@symbilityintersect.com>
2018-08-28 13:30:13 -0400

    fix domain flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ TLS_CERT=${TLS_CERT:-}
 TLS_KEY=${TLS_KEY:-}
 LOG_LEVEL=${LOG_LEVEL:-debug}
 
-exec /usr/local/bin/ngrokd -log stdout -log-level "$LOG_LEVEL" -d "$DOMAIN" -tlsCrt "$TLS_CERT" -tlsKey "$TLS_KEY"
+exec /usr/local/bin/ngrokd -log stdout -log-level "$LOG_LEVEL" -domain "$DOMAIN" -tlsCrt "$TLS_CERT" -tlsKey "$TLS_KEY"
